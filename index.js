fedc303c4fd4dba22bb4d206c44cb346ffb1425b
iamasoreloser <n10922146@gmail.com>
2026-06-24 19:40:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ At the end of every message, append a move directive (hidden from players, used 
     }
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
